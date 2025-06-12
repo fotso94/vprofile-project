6e906424909c9a345b1e3c7024891b28326fe141
fotso94 <s92fotso@gmail.com>
2025-06-12 04:27:56 -0400

    Update baseDriver.java

--- src/test/java/DevOPS/devOPS/baseDriver.java
+++ src/test/java/DevOPS/devOPS/baseDriver.java
@@ -5,23 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class baseDriver {
-	
-	public baseDriver()
-	{
-		Chromedriver();
-	}
-	
-	
-	public static WebDriver Chromedriver() {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
-				  + "\\ChromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		return driver;
 
-	}
-	
-	
-	
+    public baseDriver() {
+        Chromedriver();
+    }
 
-	
+    public static WebDriver Chromedriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
 }
