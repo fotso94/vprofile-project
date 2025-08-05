55999f43d10fc7a816e242f1687cccec3c32c43b
Stephan Money <s92fotso@gmail.com>
2025-08-05 14:38:31 -0400

    asDA

--- src/test/java/DevOPS/devOPS/baseDriver.java
+++ src/test/java/DevOPS/devOPS/baseDriver.java
@@ -2,28 +2,26 @@ package DevOPS.devOPS;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class baseDriver {
-
-	public baseDriver() {
-		// Constructor - driver initialization handled in Chromedriver() method
+	
+	public baseDriver()
+	{
+		Chromedriver();
 	}
-
+	
+	
 	public static WebDriver Chromedriver() {
-		// Use WebDriverManager to automatically handle driver setup
-		WebDriverManager.chromedriver().setup();
-
-		// Configure Chrome options for Jenkins Windows slave
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless"); // Run without GUI for Jenkins
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--window-size=1920,1080");
-
-		WebDriver driver = new ChromeDriver(options);
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
+				  + "\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		return driver;
+
 	}
+	
+	
+	
+
+	
 }
